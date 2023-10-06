8683be3d0d52a6e5229e2d9474978a4203ac029a
thddlrla <okjh0999@naver.com>
2023-10-07 04:27:30 +0900

    creat main c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+			int answer = 30; 
+	
+	//do 문장; //조건을 따지기 전에 한번
+// while ( 조건식 );
+	do {
+		printf(" guess the answer : ");
+		scanf_s("%d", &answer); 
+		
+		if(answer < 30){
+			printf("up!\n");
+		}
+		else if(answer=30) {
+			printf("congratulation!!\n");
+	      }
+	    else{
+	    	printf("down!\n");
+		}
+	  }
+	while(answer != 30);	
+         
+
+return 0;
+}
+	
+	
+
